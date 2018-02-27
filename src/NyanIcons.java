8dc8360673df4b9279574bf3fc1790cb0b93bd6b
Steve Connelly <eokuwwy@gmail.com>
2018-02-26 19:52:38 -0600

    Resized parrots to 32px

--- src/NyanIcons.java
+++ src/NyanIcons.java
@@ -3,6 +3,6 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.*;
 
 public interface NyanIcons {
-    Icon CAT_ICON = IconLoader.getIcon("/congapartyparrot.gif");
-    Icon RCAT_ICON = IconLoader.getIcon("/reversecongaparrot.gif");
+    Icon CAT_ICON = IconLoader.getIcon("/partyparrot32.gif");
+    Icon RCAT_ICON = IconLoader.getIcon("/slowparrot32.gif");
 }
